e6b27fed8a9f334d1a92962af6777f1ac8eb58d1
Yany954 <yanygonzalezyepez@gmail.com>
2023-09-19 18:49:21 -0400

    cocmmit merge

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = 3001
 const cors = require('cors');
+
 app.use(cors({
-    origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://nayasport.com.co'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   }));
 
